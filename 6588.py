9ff3427a241aaacaaf22c90d8fc88c310be675bb
rhekdms <rhekdms0507@gmail.com>
2023-06-09 10:03:10 +0900

    수정

--- 6588.py
+++ 6588.py
@@ -1,21 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-prime_num = [0,0,1]+[1,0]*(10**7//2)
+prime_num = [0,0]+[1]*(10**6)
 
-for i in range(3,10**7):
+for i in range(2,10**6):
     if prime_num[i]:
-        for j in range(i*2,10**7+1,i):
+        for j in range(i*2,10**6+1,i):
             prime_num[j]=0
 
 while True:
-    n = int(input().strip())
+    n = int(input())
     if n == 0:
         break
-    elif n % 2 == 1:
-        print("Goldbach's conjecture is wrong.")
     else:
-        for i in range(2,n):
+        for i in range(3,n//2+1,2):
             if prime_num[i] and prime_num[n-i]:
                 print(f"{n} = {i} + {n-i}")
                 break
