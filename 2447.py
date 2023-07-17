30064c0f8fff39e16443275926d0bb56df0c429b
rhekdms <rhekdms0507@gmail.com>
2023-07-18 00:26:35 +0900

    230717++

--- 2447.py
+++ 2447.py
@@ -4,6 +4,8 @@ def f(x):
     if x == 3:
         return [['*','*','*'],['*',' ','*'],['*','*','*']]
     else:
-        return [sum(i,[]) for i in zip(f(x//3),f(x//3),f(x//3))]+[sum(k,[]) for k in zip(f(x//3),[[' ' for i in range(x//3)] for j in range(x//3)],f(x//3))]+[sum(i,[]) for i in zip(f(x//3),f(x//3),f(x//3))]
+        a = [sum(i,[]) for i in zip(f(x//3),f(x//3),f(x//3))]
+        b = [sum(k,[]) for k in zip(f(x//3),[[' ' for i in range(x//3)] for j in range(x//3)],f(x//3))]
+        return a+b+a
 for i in f(N):
     print(*i,sep='')
