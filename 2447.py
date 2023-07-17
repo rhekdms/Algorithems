89c4eff32789c28987c4164d2b401a304076fb45
rhekdms <rhekdms0507@gmail.com>
2023-07-18 00:22:07 +0900

    230717+

--- 2447.py
+++ 2447.py
@@ -2,6 +2,8 @@ import sys
 N = int(sys.stdin.readline())
 def f(x):
     if x == 3:
-        return ['***','* *','***']
+        return [['*','*','*'],['*',' ','*'],['*','*','*']]
     else:
-        return [zip(f(x//3),f(x//3),f(x//3)), zip(f(x//3),[' '*(x//3) for i in range(x//3)],f(x//3)), zip(f(x//3),f(x//3),f(x//3))]
+        return [sum(i,[]) for i in zip(f(x//3),f(x//3),f(x//3))]+[sum(k,[]) for k in zip(f(x//3),[[' ' for i in range(x//3)] for j in range(x//3)],f(x//3))]+[sum(i,[]) for i in zip(f(x//3),f(x//3),f(x//3))]
+for i in f(N):
+    print(*i,sep='')
