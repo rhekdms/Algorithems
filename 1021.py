cb706b310773fc5f2c667e9f95f0239ac8ec1815
rhekdms <rhekdms0507@gmail.com>
2023-08-01 22:43:46 +0900

    230801

--- 1021.py
+++ 1021.py
@@ -1,18 +1,15 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+chess = tuple(['WBWBWBWB','BWBWBWBW']*4)
 N,M = map(int,input().split())
-num = deque(map(int,input().split()))
-N = deque(range(1,N+1))
-cnt = 0
-while num:
-    if N[0] == num[0]:
-        num.popleft()
-        N.popleft()
-    elif N.index(num[0])<=len(N)//2:
-        N.rotate(-1)
-        cnt+=1
-    else:
-        N.rotate(1)
-        cnt+=1
-print(cnt)
+board = tuple(input().strip() for i in range(N))
+sol = 32
+for i in range(N-7):
+    for j in range(M-7):
+        cnt = 0
+        for k in range(8):
+            for l in range(8):
+                if chess[k][l]!=board[k+i][l+j]:
+                    cnt+=1
+        sol = min(sol,cnt,64-cnt)
+print(sol)
---
+++ 1436.py
@@ -0,0 +1,8 @@
+import sys
+N = int(sys.stdin.readline())
+i = 666
+while N:
+    if '666' in str(i):
+        N-=1
+    i+=1
+print(i-1)
