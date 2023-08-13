3c08eebcd8fd8b0e048f0b6aab4728db606349fc
rhekdms <rhekdms0507@gmail.com>
2023-08-13 22:25:43 +0900

    230813

--- 28282.py
+++ 28282.py
@@ -1,5 +1,15 @@
 import sys
 input = sys.stdin.readline
 X,K = map(int,input().split())
-A = list(map(int,input().split()))
-print(sum(A[:X].count(i)*(X-A[X:].count(i)) for i in range(1,K+1)))
+A = [{i:0 for i in range(1,K+1)},{i:X for i in range(1,K+1)}]
+j = 1
+for i in list(map(int,input().split())):
+    if j > X:
+        A[1][i]-=1
+    else:
+        A[0][i]+=1
+    j+=1
+sol = 0
+for key,value in A[0].items():
+    sol+=A[1][key]*value
+print(sol)
